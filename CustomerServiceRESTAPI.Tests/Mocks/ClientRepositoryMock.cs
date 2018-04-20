9059daa7dba05ef42f0c1b07bff8c5df2e2558ac
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-04-20 15:50:29 -0400

    added initial mock files

---
+++ CustomerServiceRESTAPI.Tests/Mocks/ClientRepositoryMock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using CustomerServiceRESTAPI.Services;
+
+namespace CustomerServiceRESTAPI.Tests.Mocks
+{
+    public class ClientRepositoryMock : IClientRepository
+    {
+        public ClientRepositoryMock()
+        {
+        }
+    }
+}
